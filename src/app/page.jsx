dd4c0db1952e447eaa33eccc87072f58687cd164
goksinb <gbasok@gmail.com>
2025-05-05 15:57:20 +0200

    add features component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,14 @@
 import Hero from "@/components/hero/Hero";
 import Mission from "@/components/mission/Mission";
 import MeetEmmo from "@/components/meetEmmo/MeetEmmo";
+import Features from "@/components/features/features";
 
 export default function Home() {
   return (
-<>
-<Hero/>
-<Mission/>
-</>
-
+    <>
+      <Hero />
+      <Mission />
+      <Features />
+    </>
   );
 }
