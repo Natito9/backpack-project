48ffafbcb8239d1df9da29c3953f93edcece43e5
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-05-05 11:57:16 +0200

    created mission component main structure

---
+++ src/components/mission/Mission.jsx
@@ -0,0 +1,14 @@
+export default function Mission() {
+    return(
+        <section>
+            <h2>Our Mission</h2>
+            <p>We're a small team building tools for children's emotional growth. Every child deserves to understand and express their feelings in a safe, fun environment.</p>
+            <div>
+                <div className=" rounded-full bg-amber-300">
+                    <icon></icon>
+                </div>
+            </div>
+
+        </section>
+    )
+}
