2e6c1f55f0983d16b2c5eb675836833b23134c0b
Profira Rusu <profiraa@yahoo.com>
2025-05-05 16:47:48 +0200

    navbar and footer components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
 import Hero from "@/components/hero/Hero";
 import Mission from "@/components/mission/Mission";
 import MeetEmmo from "@/components/meetEmmo/MeetEmmo";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
 <>
+<Navbar/>
 <Hero/>
 <Mission/>
+<Footer/>
 </>
 
   );
