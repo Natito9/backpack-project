f93ff2e0c443a4a110b2b6a389b1867e9591076f
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-05-05 13:14:13 +0200

    added icons structure

--- src/components/mission/Mission.jsx
+++ src/components/mission/Mission.jsx
@@ -1,11 +1,46 @@
+import Image from "next/image"
+
 export default function Mission() {
     return(
-        <section>
-            <h2>Our Mission</h2>
-            <p>We're a small team building tools for children's emotional growth. Every child deserves to understand and express their feelings in a safe, fun environment.</p>
-            <div>
-                <div className=" rounded-full bg-amber-300">
-                    <icon></icon>
+        <section className="flex flex-col justify-center items-center py-20">
+            <h2 className="text-4xl pb-7">Our Mission</h2>
+            <p className="max-w-[700px] text-center pb-12">We're a small team building tools for children's emotional growth. Every child deserves to understand and express their feelings in a safe, fun environment.</p>
+            <div className="flex items-center justify-center gap-20">
+                <div>
+                <div className=" w-[80px] h-[80px] rounded-full bg-[#f1f5ff] flex justify-center items-center">
+                     <Image
+                             className="md:w-[30px] h-auto"
+                             src="/assets/icons/mission1.png"
+                             alt="Heart"
+                             width={30}
+                             height={30}
+                           />
+                </div>
+                Safe Space
+                </div>
+                <div>
+                <div className=" w-[80px] h-[80px] rounded-full bg-[#f1f5ff] flex justify-center items-center">
+                     <Image
+                             className="md:w-[30px] h-auto"
+                             src="/assets/icons/mission2.png"
+                             alt="Heart"
+                             width={30}
+                             height={30}
+                           />
+                </div>
+                Safe Space
+                </div>
+                <div>
+                <div className=" w-[80px] h-[80px] rounded-full bg-[#f1f5ff] flex justify-center items-center">
+                     <Image
+                             className="md:w-[30px] h-auto"
+                             src="/assets/icons/mission2.png"
+                             alt="Heart"
+                             width={30}
+                             height={30}
+                           />
+                </div>
+                Safe Space
                 </div>
             </div>
 
