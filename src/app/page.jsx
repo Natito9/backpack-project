77dad16dd993d380940ce1e3e7142dd784de4f43
Asher-zx <zhixuan.wang@hyperisland.se>
2025-05-05 15:00:15 +0200

    save the temporary file for the meet-emmo branch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero/Hero";
 import Mission from "@/components/mission/Mission";
+import MeetEmmo from "@/components/meetEmmo/MeetEmmo";
 
 export default function Home() {
   return (
 <>
 <Hero/>
 <Mission/>
+<MeetEmmo/>
 </>
 
   );
