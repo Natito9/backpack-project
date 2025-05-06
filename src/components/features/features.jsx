6b94903bdf2fd7b3649cd98bdc2e0907f52c6b3f
goksinb <gbasok@gmail.com>
2025-05-06 23:43:19 +0200

    fix seo on features

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -1,22 +1,23 @@
+import {Section} from "lucide-react";
 import Image from "next/image";
 
 export default function Features() {
   return (
-    <div className="bg-violet-100 font-sans mx-auto flex flex-col items-center justify-center p-5 py-20 gap-15 ">
-      <h1 className="text-3xl font-bold">How Backpack Works</h1>
+    <section className="bg-violet-100 font-content mx-auto flex flex-col items-center justify-center p-5 py-20 gap-15 ">
+      <h2 className="text-3xl font-bold">How Backpack Works</h2>
       <div className="flex flex-row ">
         <div className="flex flex-col align-center gap-8">
           <div className="flex flex-row gap-3">
             <Image
               className="rounded-xl"
               src="/assets/images/smileyIcon.png"
-              alt="image"
+              alt="Smiley Icon"
               width={50}
               height={50}
             />
             <div>
-              <h6 className="text-lg font-semibold">Daily Check-ins</h6>
-              <p className="text-gray-600">
+              <h3 className="text-title font-bold">Daily Check-ins</h3>
+              <p className="text-gray-600 text-content">
                 Emmo helps kids identify and express their daily emotions
               </p>
             </div>
@@ -25,13 +26,13 @@ export default function Features() {
             <Image
               className="rounded-xl"
               src="/assets/images/puzzleIcon.png"
-              alt="image"
+              alt="Puzzle Icon"
               width={50}
               height={50}
             />
             <div>
-              <h6 className="text-lg font-semibold">Emotional Games</h6>
-              <p className="text-gray-600">
+              <h3 className="text-title font-bold">Emotional Games</h3>
+              <p className="text-gray-600 text-content">
                 Fun activities that teach emotional intelligence
               </p>
             </div>
@@ -40,27 +41,27 @@ export default function Features() {
             <Image
               className="rounded-xl"
               src="/assets/images/questionIcon.png"
-              alt="image"
+              alt="Question Icon"
               width={50}
               height={50}
             />
             <div>
-              <h6 className="text-lg font-semibold">Emmo's room</h6>
-              <p className="text-gray-600">
+              <h3 className="text-title font-bold">Emmo's room</h3>
+              <p className="text-gray-600 text-content">
                 A personalized safe space for reflection and calm
               </p>
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-start">
+        <div className="flex flex-col">
           <Image
             src="/assets/images/appImage.png"
-            alt="image"
+            alt="Image of the app"
             width={600}
             height={600}
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
