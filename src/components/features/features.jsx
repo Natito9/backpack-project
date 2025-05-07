0a0c8f7f332928706bed9f7ae7fb5693a21cca96
goksinb <gbasok@gmail.com>
2025-05-07 22:26:27 +0200

    fix seo and add new content to features

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -1,24 +1,27 @@
-import {Section} from "lucide-react";
 import Image from "next/image";
 
 export default function Features() {
+  const iconWidth = 80;
+  const iconHeight = 80;
+
   return (
-    <section className="bg-violet-100 font-content mx-auto flex flex-col items-center justify-center p-5 py-20 gap-15 ">
-      <h2 className="text-3xl font-bold">How Backpack Works</h2>
-      <div className="flex flex-row ">
-        <div className="flex flex-col align-center gap-8">
-          <div className="flex flex-row gap-3">
+    <section className="bg-violet-100 font-content mx-auto flex flex-col items-center justify-center p-5 py-10 gap-15 ">
+      <h2 className="text-(length:--subtitle-font-size)">How Backpack Works</h2>
+      <div className="flex flex-col lg:flex-row gap-10 ">
+        <div className="flex flex-col justify-center gap-8 max-w-[600px]">
+          <div className="flex flex-row gap-3 ">
             <Image
               className="rounded-xl"
               src="/assets/images/smileyIcon.png"
               alt="Smiley Icon"
-              width={50}
-              height={50}
+              width={iconWidth}
+              height={iconHeight}
             />
             <div>
-              <h3 className="text-title font-bold">Daily Check-ins</h3>
+              <h3 className="text-lg">Engage early</h3>
               <p className="text-gray-600 text-content">
-                Emmo helps kids identify and express their daily emotions
+                Backpack is an app best suited for children aged 7-10 and is
+                meant to be implemented on their first phone.
               </p>
             </div>
           </div>
@@ -27,13 +30,14 @@ export default function Features() {
               className="rounded-xl"
               src="/assets/images/puzzleIcon.png"
               alt="Puzzle Icon"
-              width={50}
-              height={50}
+              width={iconWidth}
+              height={iconHeight}
             />
             <div>
-              <h3 className="text-title font-bold">Emotional Games</h3>
+              <h3 className="text-lg ">Personalised mascot</h3>
               <p className="text-gray-600 text-content">
-                Fun activities that teach emotional intelligence
+                The star of the app is a personalised mascot named Emmo, who
+                supports the child in real-time on the phone.
               </p>
             </div>
           </div>
@@ -42,13 +46,47 @@ export default function Features() {
               className="rounded-xl"
               src="/assets/images/questionIcon.png"
               alt="Question Icon"
-              width={50}
-              height={50}
+              width={iconWidth}
+              height={iconHeight}
             />
             <div>
-              <h3 className="text-title font-bold">Emmo's room</h3>
+              <h3 className="text-lg">Stay on track</h3>
               <p className="text-gray-600 text-content">
-                A personalized safe space for reflection and calm
+                Emmo specializes in micro-interventions, nudging, emotional
+                maturity, building self awareness, and maintaining boundaries.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-3">
+            <Image
+              className="rounded-xl"
+              src="/assets/images/questionIcon.png"
+              alt="Question Icon"
+              width={iconWidth}
+              height={iconHeight}
+            />
+            <div>
+              <h3 className="text-lg">Invite play</h3>
+              <p className="text-gray-600 text-content">
+                Emmo can suggest fun activities to do in the physical world,
+                actively shifting focus from the screen to
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-3">
+            <Image
+              className="rounded-xl"
+              src="/assets/images/questionIcon.png"
+              alt="Question Icon"
+              width={iconWidth}
+              height={iconHeight}
+            />
+            <div>
+              <h3 className="text-lg">Grow closer</h3>
+              <p className="text-gray-600 text-content">
+                Additionally, Emmo creates a communicative bridge between parent
+                and child, keeping the parent up to date with overall screen
+                habits.
               </p>
             </div>
           </div>
@@ -57,8 +95,9 @@ export default function Features() {
           <Image
             src="/assets/images/appImage.png"
             alt="Image of the app"
-            width={600}
-            height={600}
+            width={700}
+            height={700}
+            className=" self-center"
           />
         </div>
       </div>
