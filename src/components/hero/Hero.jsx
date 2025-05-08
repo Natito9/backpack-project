e1f2d7299f327e504e19eac0ef865f643e174515
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-05-08 14:25:16 +0200

    added all images in hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,17 +3,34 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className=" md:h-auto bg-(--bg-color) flex justify-center">
+    <section className="relative  md:h-screen flex justify-center  w-full"
+>
+       <Image
+                src="/assets/images/hero-background.jpg"
+                alt="Background"
+                fill
+                priority
+                className="object-cover object-center -z-10"
+              />
 
     
-      <div className="flex justify-right items-right pt-10">
+      <div className=" w-full">
+      <div className="w-1/2 flex justify-center items-center">
+        <Image
+          className="h-auto "
+          src="/assets/images/hero-logo.png"
+          alt="Bakcpack Studios logo"
+          width={500}
+          height={400}
+        />
+      </div>
       <div className="w-52 md:w-[500px] ">
         <Image
           className="h-auto "
           src="/assets/images/hero-emmo.png"
           alt="Emmo character"
-          width={400}
-          height={400}
+          width={300}
+          height={300}
         />
       </div>
       </div>
