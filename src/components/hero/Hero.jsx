9f5c6dd293ea79222ec0133b454118c282818b42
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-05-08 13:46:15 +0200

    deleted text from hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,23 @@
 import Image from "next/image";
-import HeroText from "./HeroText";
+
 
 export default function HeroSection() {
   return (
     <section className=" md:h-auto bg-(--bg-color) flex justify-center">
-      <div className="max-w-[1440px] flex flex-col  md:flex-row justify-between px-5 py-10 md:pl-20 md:py-30 "> 
-      <HeroText/>
-      <div className="flex justify-center items-center pt-10">
+
+    
+      <div className="flex justify-right items-right pt-10">
       <div className="w-52 md:w-[500px] ">
         <Image
           className="h-auto "
-          src="/assets/images/hero.png"
+          src="/assets/images/hero-emmo.png"
           alt="Emmo character"
           width={400}
           height={400}
         />
       </div>
       </div>
-      </div>
+     
     </section>
   );
 }
