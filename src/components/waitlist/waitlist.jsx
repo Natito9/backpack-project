aa3a7ff8771dd964e8d1ba42da18f54343addc95
Swathi S Menon <swathismenon@Swathis-MacBook-Air.local>
2025-05-08 20:11:45 +0200

    updated

--- src/components/waitlist/waitlist.jsx
+++ src/components/waitlist/waitlist.jsx
@@ -1,55 +1,39 @@
+'use client'; 
+
 import React from "react";
 
 export default function Waitlist() {
   return (
-    <section
-      style={{ backgroundColor: "var(--main-color)" }}
-      className="text-black py-20 text-center"
-    >
+    <section className="bg-[var(--main-color)] text-black py-20 text-center">
       <div className="max-w-2xl mx-auto px-4">
-        <h2
-          className="text-2xl sm:text-3xl font-semibold leading-tight font-subtitle"
-          style={{
-            fontFamily: "var(--subtitle-font-family)",
-            color: "var(--text-color)",
-          }}
-        >
+        <h2 className="text-2xl sm:text-3xl font-semibold leading-tight font-subtitle text-[var(--text-color)]">
+          
           Join the Movement
+
         </h2>
-        <p
-          className="mt-4 text-placeholder leading-normal font-placeholder"
-          style={{
-            fontFamily: "var(--content-font-family)",
-            color: "var(--text-color)",
-          }}
-        >
+        <p className="mt-4 text-placeholder leading-normal font-placeholder text-[var(--text-color)]">
           Be part of something special. Help us create a world where every child{" "}
           <br />
           has the tools to understand and express their emotions.
         </p>
+
         <form className="mt-8 flex flex-col sm:flex-row items-center justify-center gap-4">
           <input
             type="email"
             placeholder="Enter your email"
-            className="w-full sm:w-[300px] px-6 py-1.5 rounded-full bg-white text-black 
-              placeholder:text-gray-400 font-placeholder text-base shadow-sm border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[--main-color]"
-            style={{
-              fontFamily: "var(--placeholder-font-family)",
-              fontSize: "var(--placeholder-font-size)",
-            }}
+            className="w-full sm:w-[300px] px-6 py-1.5 rounded-full bg-white text-black placeholder:text-gray-400 font-placeholder text-base shadow-sm border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[var(--main-color)]"
             required
           />
 
-<button
-  type="submit"
-  className="px-8 py-2 rounded-full bg-white font-semibold shadow-sm text-black cursor-pointer hover:bg-[#cde5e2]"
-  style={{
-    fontFamily: "var(--button-font-family)",
-    fontSize: "var(--button-font-size)",
-  }}
->
-  Join Waitlist
-</button>
+          <button
+            type="submit"
+            className="px-8 py-2 rounded-full bg-white font-semibold shadow-sm text-black cursor-pointer hover:bg-[#cde5e2]"
+          >
+
+            Join Waitlist
+
+          </button>
+          
         </form>
       </div>
     </section>
