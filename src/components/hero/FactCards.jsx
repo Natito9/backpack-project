67f859f585fa3771434270aff078c04bdb2f984a
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-05-09 09:29:23 +0200

    removed jey frame from tailwing config and use library css.aniamte instead

--- src/components/hero/FactCards.jsx
+++ src/components/hero/FactCards.jsx
@@ -19,17 +19,18 @@ export default function FactCards({ onDone }) {
   const facts = ['1/4 year', '3 months', '13 weeks', '92 days', '2 200h']
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center text-white">
+    <div className="absolute inset-0 flex items-center justify-center ">
       {facts.map((fact, i) => {
         const offset = (i - Math.floor(facts.length / 2)) * 150
 
         return (
           <div
             key={i}
-            className="absolute w-[20vw] h-[20vw] min-w-[100px] min-h-[100px] max-w-[200px] max-h-[200px]
+            className={`absolute w-[20vw] h-[20vw] min-w-[100px] min-h-[100px] max-w-[200px] max-h-[200px]
               flex items-center justify-center text-center rounded-full bg-white text-black
               font-bold text-2xl md:text-3xl transition-all duration-700 ease-in-out
-              top-1/2 -translate-y-1/2"
+              top-1/2 -translate-y-1/2 ${i === 0 ? 'animate__animated animate__infinite animate__slow animate__pulse' : ''}`}
+            
             style={{
               transform: expanded
                 ? `translate(${offset}px, -50%)`
