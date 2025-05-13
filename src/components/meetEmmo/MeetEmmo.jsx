f0e23570ad7a326300cba5cdc178c4037f55a28c
Asher-zx <zhixuan.wang@hyperisland.se>
2025-05-13 23:28:57 +0200

    font line adjustments

--- src/components/meetEmmo/MeetEmmo.jsx
+++ src/components/meetEmmo/MeetEmmo.jsx
@@ -5,7 +5,7 @@ const MeetEmmo = () => {
   return (
     <section 
       id='emmo' 
-      className="flex flex-col md:flex-row items-center justify-center max-w-7xl mx-auto py-16 px-4 gap-8">
+      className="flex flex-col md:flex-row items-center justify-center max-w-8xl mx-auto py-16 px-4 gap-8">
       {/* Image card */}
       <div className="rounded-3xl overflow-hidden shadow-lg md:w-1/3 flex items-center justify-center p-8">
         <Image 
@@ -22,11 +22,8 @@ const MeetEmmo = () => {
           Meet Emmo
         </h2>
 
-        <p className="text-(length:--content-font-size) md:text-[20px] leading-[24px] md:leading-[28px] font-(family-name:--title-font-family) w-full md:w-[576px] text-center md:text-left whitespace-pre-line">
-          Emmo is more than just a character - they're a trusted friend 
-          who helps kids navigate their emotional journey. With a heart 
-          full of empathy and a backpack full of tools, Emmo makes 
-          learning about feelings fun and accessible.
+        <p className="text-(length:--content-font-size) md:text-[20px] leading-[24px] md:leading-[28px] font-(family-name:--title-font-family) w-full text-justify hyphens-auto">
+        Emmo is the antidote to all the negatively conditioning algorithms that target children today. That turns them into infinity scrollers. Causes gaming addiction. Harms their self image and makes them go Emmo does not substitute a parent, a friend or functions like a tamagotchi. You don't have to feed Emmo and will not be invited to check on the Mascot when you are off the phone. Emmo helps the children learn about themselves, their emotions and behaviours. So that they can independently with old a healthy relationship to digital world in the future.after instant dopamine kicks.Emmo is the gatekeeper that creates space for emotional growth, an active lifestyle, a solid self esteem and understanding the importance of boundries as the children grow. Space that now is eaten up by screen time. 
         </p>
 
         <div className="flex flex-wrap gap-4 mt-6 justify-center md:justify-start">
