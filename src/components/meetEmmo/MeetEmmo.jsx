b7dacd3902d3a2066a2f2840906edac5b67ea7f7
Asher-zx <zhixuan.wang@hyperisland.se>
2025-05-15 11:23:21 +0200

    bug fixed

--- src/components/meetEmmo/MeetEmmo.jsx
+++ src/components/meetEmmo/MeetEmmo.jsx
@@ -7,7 +7,7 @@ const MeetEmmo = () => {
       id='emmo' 
       className="flex flex-col md:flex-row items-center justify-center max-w-8xl mx-auto py-16 px-4 gap-8">
       {/* Image card */}
-      <div className="rounded-3xl overflow-hidden shadow-lg md:w-1/3 flex items-center justify-center p-8">
+      <div className="rounded-3xl overflow-hidden shadow-lg flex items-center justify-center p-8">
         <Image 
           src="/assets/images/emmo.png"
           alt="Emmo character"
@@ -17,12 +17,12 @@ const MeetEmmo = () => {
         />
       </div>
 
-      <div className="w-full md:w-1/2 space-y-6 flex flex-col justify-center">
+      <div className="space-y-6 flex flex-col justify-center">
         <h2 className="text-(length:--subtitle-font-size) md:leading-[60px] text-center md:text-left leading-title font-(family-name:--title-font-family)">
           Meet Emmo
         </h2>
 
-        <p className="font-(family-name:--title-font-family) text-justify text-[#3f3f3f]">
+        <p className="max-w-[750px] font-(family-name:--title-font-family) text-justify text-[#3f3f3f]">
           Emmo is here to protect kids from the pull of addictive algorithms - the kind that fuel endless scrolling, gaming loops, and low self-worth.
           <br />Emmo isn't a toy, a pet, or a digital friend. It's a gentle guide that helps kids understand their feelings, habits, and needs - and build self-regulation from within.
           <br />By creating space for movement, confidence, and emotional growth, Emmo supports the skills children need to stay balanced in a digital world.        
